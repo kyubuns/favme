e6d61addb24f8af912f2ba69a813e0c87fe50600
kyubuns <jp@ex.cx>
2012-08-16 10:02:08 +0900

    css対策追加

--- favme.py
+++ favme.py
@@ -1,14 +1,18 @@
+# -*- coding: utf-8 -*-
 from flask import request
 from jinja2 import Environment, Template
 from xml.sax.saxutils import *
 import jinja2
+import copy
 
 class UserInputString:
   def __init__(self, message):
     self._message = message
     self.escapeHTML = False
     self.escapeJS = False
+    self.escapeCSS = False
     self.rawHTML = False
+    print "create"
 
   def __eq__(self, i):
     return self._message == i
@@ -16,28 +20,37 @@ class UserInputString:
   def __ne__(self, i):
     return self._message != i
 
+  def __len__(self):
+    return len(self._message)
+
   def getRawString(self):
     return self._message
 
   def __str__(self):
     if self.rawHTML == False and self.escapeHTML == False and self.escapeJS == False:
-      raise TypeError, ''
+      raise TypeError, 'hoge'
 
     tmp = self._message
     if self.escapeHTML == True:
       tmp = escape(tmp)
     if self.escapeJS == True:
       tmp = js_escape(tmp)
+    if self.escapeCSS == True:
+      tmp = tmp.lower()
+      tmp = tmp.replace("expression", "")
     return tmp
 
-  def js_escape(src):
-    return src.translate({
-      ord(u"'"): u"\'",
-      ord(u'"'): u'\"',
-      ord(u"\\"): u"\\\\",
-      ord(u'/'): u'\/',
-      ord(u'>'): u'\x3e'
-    })
+def js_escape(src):
+  if len(src) == 0:
+    return src
+  return src.translate({
+    ord(u"'"): u"\\\'",
+    ord(u'"'): u"\\\"",
+    ord(u"\\"):u"\\\\",
+    ord(u'/'): u"\\/",
+    ord(u'>'): u"\\x3e",
+    ord(u':'): u"\\:"
+  })
 
 class FavEnvironment(Environment):
   def get_template(self, filename):
@@ -58,22 +71,43 @@ class FavTemplate:
 def favreq(name, default=""):
   return UserInputString(request.values.get(name, default))
 
+def tounicode(data):
+  codecs = ['shift_jis','utf-8','euc_jp','cp932',
+            'euc_jis_2004','euc_jisx0213','iso2022_jp','iso2022_jp_1',
+            'iso2022_jp_2','iso2022_jp_2004','iso2022_jp_3','iso2022_jp_ext',
+            'shift_jis_2004','shift_jisx0213','utf_16','utf_16_be',
+            'utf_16_le','utf_7','utf_8_sig'];
+  for codec in codecs:
+    try: return data.decode(codec);
+    except: continue;
+  return u"";
+
 ##########################################################
 
-def raw_filter(s):
+def raw_filter(m):
+  s = copy.deepcopy(m)
   if s.__class__.__name__ == "UserInputString":
     s.rawHTML = True
   return s
 jinja2.filters.FILTERS['raw'] = raw_filter
 
-def html_filter(s):
+def html_filter(m):
+  s = copy.deepcopy(m)
   if s.__class__.__name__ == "UserInputString":
     s.escapeHTML = True
   return s
 jinja2.filters.FILTERS['html'] = html_filter
 
-def js_filter(s):
+def js_filter(m):
+  s = copy.deepcopy(m)
   if s.__class__.__name__ == "UserInputString":
     s.escapeJS = True
   return s
 jinja2.filters.FILTERS['js'] = js_filter
+
+def css_filter(m):
+  s = copy.deepcopy(m)
+  if s.__class__.__name__ == "UserInputString":
+    s.escapeCSS = True
+  return s
+jinja2.filters.FILTERS['css'] = css_filter
--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 from jinja2 import FileSystemLoader
 from favme import FavTemplate, FavEnvironment, favreq
@@ -8,7 +9,11 @@ env = FavEnvironment(loader=FileSystemLoader("."))
 @app.route("/", methods=['GET','POST'])
 def add_user():
   template = env.get_template("test.html")
-  return template.render(name=favreq("name"))
+  inputname = favreq("name")
+  if inputname:
+    return template.render(name=inputname)
+  else:
+    return template.render()
 
 if __name__ == "__main__":
   app.run(debug=True)
